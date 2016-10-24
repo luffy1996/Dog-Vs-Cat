274d60fa8d55f374c7b24f544f758e461bad0770
rohan <rajrohan1108@gmail.com>
2016-10-24 23:56:09 +0530

    I have added some of the import packages

---
+++ 150107048_assignment2_DogCat.py
@@ -0,0 +1,12 @@
+'''This is assignment 2 , from Advanced Machine Learning Course by Professor Amit Sethi'''
+from __future__ import print_function
+import numpy as np
+np.random.seed(2001)
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten , Convolution2D, Maxpooling2D
+from keras.utils import np_utils
+from keras import backend as K
+
+
+
